b6116f763f04d36a550af3288337db0a8713e83d
Vitaly Kovalev <kovalev.vitaly.msu@yandex.ru>
2026-04-08 16:39:52 +0300

    fix: add mock users data to resolve foreign key conflicts
    Change PlayerID type to uuid.UUID.

--- internal/repository/mappers/mappers.go
+++ internal/repository/mappers/mappers.go
@@ -3,7 +3,6 @@ package mappers
 import (
 	repoModel "board-games/internal/repository/model"
 	ucModel "board-games/internal/usecases/model"
-	"strconv"
 	"strings"
 )
 
@@ -48,33 +47,33 @@ var gameTypes = map[int]ucModel.GameType{
 }
 
 // Deprecated
-func ToGameInfo(data repoModel.GameData) *ucModel.GameInfo {
-	return &ucModel.GameInfo{
-		ID:             data.ID,
-		Type:           gameTypes[data.NameID],
-		BoardWidth:     data.BoardWidth,
-		BoardHeight:    data.BoardHeight,
-		Players:        data.Players,
-		Moves:          parseMoves(data.Moves),
-		Winner:         data.Winner,
-		AdditionalInfo: data.AdditionalInfo,
-	}
-}
+// func ToGameInfo(data repoModel.GameData) *ucModel.GameInfo {
+// 	return &ucModel.GameInfo{
+// 		ID:             data.ID,
+// 		Type:           gameTypes[data.NameID],
+// 		BoardWidth:     data.BoardWidth,
+// 		BoardHeight:    data.BoardHeight,
+// 		Players:        data.Players,
+// 		Moves:          parseMoves(data.Moves),
+// 		Winner:         data.Winner,
+// 		AdditionalInfo: data.AdditionalInfo,
+// 	}
+// }
 
 // Deprecated
 // Parse string in format:
 //
 // "[playerID]@[MoveRepr]#..."
-func parseMoves(s string) []ucModel.MoveInfo {
-	moveReprs := strings.Split(s, MoveInfoDelimeter)
-	moves := make([]ucModel.MoveInfo, len(moveReprs))
-	for _, mvRepr := range moveReprs {
-		l := strings.Split(mvRepr, PlayerDelimeter)
-		playerID, _ := strconv.ParseInt(l[0], 10, 64)
-		moves = append(moves, ucModel.MoveInfo{
-			PlayerID: ucModel.PlayerID(playerID),
-			MoveRepr: l[1],
-		})
-	}
-	return moves
-}
+// func parseMoves(s string) []ucModel.MoveInfo {
+// 	moveReprs := strings.Split(s, MoveInfoDelimeter)
+// 	moves := make([]ucModel.MoveInfo, len(moveReprs))
+// 	for _, mvRepr := range moveReprs {
+// 		l := strings.Split(mvRepr, PlayerDelimeter)
+// 		playerID, _ := strconv.ParseInt(l[0], 10, 64)
+// 		moves = append(moves, ucModel.MoveInfo{
+// 			PlayerID: ucModel.PlayerID(playerID),
+// 			MoveRepr: l[1],
+// 		})
+// 	}
+// 	return moves
+// }
